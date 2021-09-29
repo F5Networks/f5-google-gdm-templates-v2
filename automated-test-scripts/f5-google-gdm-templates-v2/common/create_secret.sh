f320ddecb46dd04aefbc6781b45fae36bfcef40f
kashcheev <a.kashcheev@f5.com>
2021-09-29 16:45:48 +0000

    Release commited to v1.0.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/create_secret.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "Created"
-#  scriptTimeout = 5
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud secrets create <STACK NAME>-secret
--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_instance_group.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "done"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-printf 'yes' | gcloud compute instance-groups managed delete <UNIQUESTRING>-bigip-ig --region <REGION>
