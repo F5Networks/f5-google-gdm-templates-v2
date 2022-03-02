1ea66b4320986bada6fc861e84d26e1416e1194c
shimkus <m.shimkus@f5.com>
2022-03-02 21:20:25 +0000

    Release commited to v2.0.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/create_target_pool.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#  expectValue = "Created"
-#  scriptTimeout = 5
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud compute target-pools create <UNIQUESTRING>-bigip-tp \
-    --region <REGION>
