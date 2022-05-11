78ae85fc9b31a2681104fc86ff9a70222cc5d645
shimkus <m.shimkus@f5.com>
2022-05-11 20:57:56 +0000

    Release commited to v2.1.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/create_secret.sh
+++
@@ -1,8 +0,0 @@
-#  expectValue = "Created"
-#  scriptTimeout = 5
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud secrets create <STACK NAME>-secret
-
-echo -n "<AUTOFILL PASSWORD>" | gcloud secrets versions add <STACK NAME>-secret --data-file=-
--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_access_stack.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "completed successfully"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud deployment-manager deployments delete <ACCESS STACK NAME> -q
