8a107ba75cdc6db8299588ab371cdf7f39756273
shimkus <m.shimkus@f5.com>
2022-07-26 20:04:35 +0000

    Release commited to v2.4.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_network_stack.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "completed successfully"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud deployment-manager deployments delete <NETWORK STACK NAME> -q
