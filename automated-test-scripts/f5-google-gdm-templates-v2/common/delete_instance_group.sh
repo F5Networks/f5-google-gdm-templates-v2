90f7c70525733db3b2fce26ebf6b59488021a92a
shimkus <m.shimkus@f5.com>
2023-02-28 20:28:28 +0000

    Release commited to v2.7.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_instance_group.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "Deleted"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud compute instance-groups managed delete <UNIQUESTRING>-bigip-ig --region <REGION> -q
--- automated-test-scripts/f5-google-gdm-templates-v2/common/verify_delete_storage.sh
+++
@@ -1,9 +0,0 @@
-#  expectValue = "completed successfully"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-bucket=`gsutil ls | grep '<STACK NAME>-bucket'`
-
-if [ $? -eq 1 ]; then
-    echo "completed successfully"
-fi
