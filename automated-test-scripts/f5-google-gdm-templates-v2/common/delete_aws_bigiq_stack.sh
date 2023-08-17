2886ce750db359f6ed3eeb35a6b657fafc619df9
shimkus <m.shimkus@f5.com>
2023-08-17 18:30:06 +0000

    Release commited to v2.9.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_aws_bigiq_stack.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region us-west-1 --stack-name <STACK NAME>-bigiq-gcp-test
-echo "PASS"
--- automated-test-scripts/f5-google-gdm-templates-v2/common/verify_delete_secret.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "not found"
-#  scriptTimeout = 2
-#  replayEnabled = true
-#  replayTimeout = 5
-
-gcloud secrets describe <STACK NAME>-secret
