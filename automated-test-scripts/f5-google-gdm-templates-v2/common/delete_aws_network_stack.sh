d543fb234672ec2246ddaa512c69a5d940a24217
shimkus <m.shimkus@f5.com>
2024-04-17 15:00:20 +0000

    Release commited to v3.1.0.0 tag

--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_aws_network_stack.sh
+++
@@ -1,8 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-aws cloudformation delete-stack --region us-west-1 --stack-name <STACK NAME>-gcp-test
-# If using PublicIP, need to also delete bastion host
-echo "PASS"
--- automated-test-scripts/f5-google-gdm-templates-v2/common/delete_target_pool.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "Deleted"
-#  scriptTimeout = 5
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud compute target-pools delete <UNIQUESTRING>-bigip-tp --region <REGION> -q
